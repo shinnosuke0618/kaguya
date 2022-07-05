a86136764c6c0c42eb198687feed07ddfb786fe4
shinnosuke0618 <s.t061825@gmail.com>
2022-07-05 17:00:41 +0000

    [add]admin_controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,25 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    #@order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    @order = Order.find_by(id: @order_detail.order_id)
+    @order_details = @order.order_details.all
+      if @order_details.where(production_status: 2).any? #もしも一つでも存在したら？
+      @order.update(status: 2)
+      redirect_to admin_order_path(@order)
+    #binding.pry
+      elsif @order_details.where(production_status: 3).all?#もしも全部だったら？
+      @order.update(status: 3)
+      redirect_to admin_order_path(@order)
+      else
+      redirect_to admin_order_path(@order)
+      end
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:production_status)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,27 @@
 class Admin::OrdersController < ApplicationController
+
+  def index
+    @orders = Order.page(params[:page])
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+     if @order.status == "confirm_payment"#入金確認
+      @order_details.update_all(production_status: 1)
+     end
+      redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
