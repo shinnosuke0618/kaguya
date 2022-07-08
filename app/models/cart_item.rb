22a33a061573c94408a1b5816a51aca29cfb73c6
shinnosuke0618 <s.t061825@gmail.com>
2022-07-08 15:51:02 +0000

    [add]model_customer

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,14 @@
 class CartItem < ApplicationRecord
+
+  belongs_to :customer
+  belongs_to :item
+
+  validates :quantity, presence: true
+
+
+  # 小計を求めるメソッド
+  def subtotal
+    item.add_tax_price * quantity
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,22 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :addresses, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :post_code, presence: true, length: {maximum: 7, minimum: 7}, numericality: true #numericality:属性に数値のみが使えるバリデーション
+  validates :address, presence: true
+  validates :phone_number, presence: true, length: {maximum: 11, minimum: 10}, numericality: true
+
+  # orderモデルで使用します。〒OOO-OOO 住所 宛名
+  def address_display
+    '〒' + post_code + ' ' + address + ' ' + last_name + first_name
+  end
+
 end
