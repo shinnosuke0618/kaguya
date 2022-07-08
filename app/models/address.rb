771e7e99a067f20465aa95ee27128dc6bcd9ead7
shinnosuke0618 <s.t061825@gmail.com>
2022-07-08 15:49:19 +0000

    [add]model_address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,15 @@
 class Address < ApplicationRecord
+  belongs_to :customer
+
+  validates :post_code, presence: true, length: { maximum: 7, minimun: 7 },numericality: true
+  validates :address, presence: true
+  validates :name, presence: true
+
+
+
+
+  # orderモデルで使用します。〒OOO-OOO 住所 宛名
+  def address_display
+  '〒' + post_code + ' ' + address + ' ' + name
+  end
 end
